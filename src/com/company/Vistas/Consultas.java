f010764bd6b4e456c1e688bd79833191c713d669
Maria <mariarc_trabajo@hotmail.com>
2021-11-07 14:04:04 +0100

    panel consultas

---
+++ src/com/company/Vistas/Consultas.java
@@ -0,0 +1,27 @@
+package com.company.Vistas;
+
+import javax.swing.*;
+
+public class Consultas {
+    private JPanel JPGeneral;
+    private JPanel JPVacio;
+    private JTextField TFConsultaBusqueda;
+    private JButton JButtonConsultaBuscar;
+    private JComboBox JComboConsultaResultado;
+    private JPanel JPConsultasBusqueda;
+    private JPanel JPConsultasCombo;
+    private JPanel JPConsultasData;
+    private JLabel lbConsultaBusqueda;
+    private JPanel JPConsultaCodigo;
+    private JPanel JPConsultaNombre;
+    private JPanel JPConsultaData1;
+    private JPanel JPConsultaDireccion;
+    private JLabel lbConsultaFixCodigo;
+    private JLabel lbConsultaFixNombre;
+    private JLabel lbConsultaFixData1;
+    private JLabel lbConsultaFixDireccion;
+    private JLabel lbConsultaResultCodigo;
+    private JLabel lbConsultaResultNombre;
+    private JLabel lbConsultaResultData1;
+    private JLabel lbConsultaResultDireccion;
+}
