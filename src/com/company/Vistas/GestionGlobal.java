44b19b57b532af95e6f66618560f2e13d638ecd4
Maria <mariarc_trabajo@hotmail.com>
2021-11-07 22:37:45 +0100

    gestion global vista

---
+++ src/com/company/Vistas/GestionGlobal.java
@@ -0,0 +1,67 @@
+package com.company.Vistas;
+
+import javax.swing.*;
+
+public class GestionGlobal {
+    private JPanel JPGeneral;
+    private JPanel JPVacio;
+    private JButton JButtonGestionGlobalInsertar;
+    private JButton JButtonGestionGlobalModificar;
+    private JButton JButtonGestionGlobalEliminar;
+    private JButton JButtonGestionGlobalListar;
+    private JComboBox CBGBProveedor;
+    private JComboBox CBGBPieza;
+    private JComboBox CBGBProyecto;
+    private JTextField TFGBCantidad;
+    private JTextField TFGBDataProveedorNombre;
+    private JTextField TFGBDataProveedorApellido;
+    private JTextField TFGBDataPiezaNombre;
+    private JTextField TFGBDataPiezaPrecio;
+    private JTextField TFGBDataProyectoNombre;
+    private JTextField TFGBDataProyectoCiudad;
+    private JPanel JPGestionGlobalButtons;
+    private JPanel JPGestionGlobal;
+    private JLabel lbGestionGlobal;
+    private JPanel JPGestioGlobalData;
+    private JPanel JPGBProveedor;
+    private JPanel JPGBPieza;
+    private JPanel JPGBProyecto;
+    private JPanel JPGBCantidad;
+    private JLabel lbGBProveedor;
+    private JPanel JPGBDataProveedor;
+    private JPanel JPGBDataProveedorNombre;
+    private JPanel JPGBDataProveedorApellido;
+    private JLabel lbGBDataProveedorNombre;
+    private JLabel lbGBDataProveedorApellido;
+    private JLabel lbGBPieza;
+    private JLabel lbGBProyecto;
+    private JLabel lbGBCantidad;
+    private JPanel JPGBDataPieza;
+    private JPanel JPGBDataProyecto;
+    private JPanel JPGBDataPiezaNombre;
+    private JPanel JPGBDataPiezaPrecio;
+    private JPanel JPGBDataProyectoNombre;
+    private JPanel JPGBDataProyectoCiudad;
+    private JLabel lbGBDataPiezaNombre;
+    private JLabel lbGBDataPiezaPrecio;
+    private JLabel lbGBDataProyectoNombre;
+    private JLabel lbGBDataProyectoCiudad;
+
+
+    public GestionGlobal() {
+    }
+
+    public JPanel getJPGeneral() {
+        return JPGeneral;
+    }
+
+    public JPanel getJPVacio() {
+
+        return JPVacio;
+
+    }
+
+
+
+
+}
